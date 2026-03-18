16de9ccc660453913128a45dfcb3780177da6714
Baptista Valeta <gabryelvaleta@gmail.com>
2026-03-18 23:15:28 +0100

    Update project. Estatics tasks

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express();
+
+app.models = {}; // Iniciar o app.modules
+
+consign()
+    .include("src/models")
+    .then("src/libs/middlewares.js")
+    .then("src/routes")
+    .then("src/libs/boot.js")
+    .into(app);
