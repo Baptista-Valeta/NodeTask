0874addb18b47aaea75d231801001d666df4d8f2
Baptista Valeta <gabryelvaleta@gmail.com>
2026-03-22 19:12:47 +0100

    Modificação de Integração com o banco de dados

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.models = {}; // Iniciar o app.modules
 
 consign()
-    .include("src/models")
+    .include("src/database.js")()
+    .then("src/models")
     .then("src/libs/middlewares.js")
     .then("src/routes")
     .then("src/libs/boot.js")
--- src/libs/boot.js
+++ src/libs/boot.js
@@ -1,5 +1,10 @@
 module.exports =  app => {
-    app.listen(3000, () => {
-        console.log(`NodeTask Rodando na porta ${app.get("port")}`);
+    // Criar tabelas e sicronizar modelos com o banco através do método sync do sequilize
+    app.database.sync()
+        .then(() => {
+            app.listen(app.get("port"), () => {
+                console.log(`NodeTask Rodando na porta ${app.get("port")}`);
+            });
+        .catch(err => console.error(err));
     });
 }
