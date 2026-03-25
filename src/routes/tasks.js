2df939c9af5232bea6ce05769ad6e4583ef86175
Baptista Valeta <gabryelvaleta@gmail.com>
2026-03-25 23:32:00 +0100

    Modelo definido com sua respectiva tabela no banco

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,42 @@
-module.exports =  app => {
-    const Tasks = app.models.tasks;
-
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}, (tasks) => {
-            res.json({tasks: tasks})
-        });
+module.exports =  (sequelize, Datatype) => {
+    // Cria tabela Tasks
+    const Tasks = sequelize.define("Tasks", {
+        id: {
+            type: Datatype.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: Datatype.STRING,
+            allowNull: false, //  pode ser vazia - não
+            validate: {
+                notEmpty: true, // não pode ser string vazia
+            }
+        },
+        done: {
+            type: Datatype.BOOLEAN,
+            allowNull: false,
+            defaultValue: false // Estado inicial da task (não concluída)
+        }, 
+        
+        // classMethods: {
+        //     associate: (models) => {
+        //         Tasks.belongsTo(models.Users); 
+        //     }
+        // }
     });
+
+    Tasks.associate = (models) => {
+        Tasks.belongsTo(models.Users); // Define o relacionamento - cada task pertence a um user
+    }
+
+    return Tasks; // Exporta o modelo tasks para uso no projecto
+    
+    // const Tasks = app.models.tasks;
+
+    // app.get("/tasks", (req, res) => {
+    //     Tasks.findAll({}, (tasks) => {
+    //         res.json({tasks: tasks})
+    //     });
+    // });
 };
