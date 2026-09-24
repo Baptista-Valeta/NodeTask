901ce7309d788a12074d96cb665105fe860a4ca1
Baptista Valeta <gabryelvaleta@gmail.com>
2026-09-24 18:49:32 +0100

    Componentes para gerenciar os componentes das rotas públicas(login e cadastro) e privadas

--- ntask-web/src/app/public-routing/login/login.component.ts
+++ ntask-web/src/app/public-routing/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(protected authService: AuthService) {}
+
+  login() {
+    console.log(this.authService.credential_form.value);
+  }
 }
